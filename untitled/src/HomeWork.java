c35be5ed08260ddcaf39845b4266087b46fba1dc
ibragimgabb <ibragimgabb@gmail.com>
2025-10-10 09:37:06 +0400

    HomeTask v1

--- untitled/src/HomeWork.java
+++ untitled/src/HomeWork.java
@@ -1,23 +1,23 @@
 
 public class HomeWork {
     public static void main(String[] args) {
+
         // 1. Declare an int primitive variable and an Integer wrapper variable
         int primitiveInt = 10;
-        Integer wrapperInt;
+        Integer wrapperInteger;
 
         // 2. Assign the int value to the Integer variable (autoboxing)
-        wrapperInt = primitiveInt; 
-
+        wrapperInteger = primitiveInt;
         // 3. Declare another Integer wrapper variable and assign it a value
         Integer anotherWrapper = 25;
 
         // 4. Assign the Integer value to an int variable (unboxing)
         int anotherPrimitive = anotherWrapper;
 
-        System.out.println("Primitive int value: " + primitiveInt);
-        System.out.println("Wrapper Integer value (autoboxed): " + wrapperInt);
-        System.out.println("Another Wrapper Integer value: " + anotherWrapper);
-        System.out.println("Another Primitive int value (unboxed): " + anotherPrimitive);
+        // Print the results
+        System.out.println("Primitive int: " + primitiveInt);
+        System.out.println("Wrapper Integer (after autoboxing): " + wrapperInteger);
+        System.out.println("Another Wrapper Integer: " + anotherWrapper);
+        System.out.println("Another Primitive int (after unboxing): " + anotherPrimitive);
     }
 }
-
