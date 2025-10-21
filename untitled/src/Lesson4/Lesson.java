a193690cc94c042c707fc07514cf7bc89b41d304
ibragimgabb <ibragimgabb@gmail.com>
2025-10-21 16:40:50 +0400

    Homework task4

---
+++ untitled/src/Lesson4/Lesson.java
@@ -0,0 +1,16 @@
+package Lesson4;
+
+public class Lesson {
+
+    static void main() {
+        double a= 5;
+        double d=8;
+        double netice = topla(a,d);
+        System.out.println("netice is " + netice);
+    }
+
+    public static double topla(double a, double b) {
+        double c = a + b;
+        return c;
+    }
+}
